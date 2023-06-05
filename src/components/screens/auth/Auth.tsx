ecebdbe0218bb832c8cee29ebcde5014876e88d1
Андрей Беляев <andrejbelaev@MacBook-Air-Andrej.local>
2023-06-05 23:10:05 +0500

    auth logics and auth page(auth forms and hooks)

--- src/components/screens/auth/Auth.tsx
+++ src/components/screens/auth/Auth.tsx
@@ -2,15 +2,23 @@ import { FC, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 import Heading from '@/ui/Heading'
+import Loader from '@/ui/Loader'
 import Meta from '@/ui/Meta'
 import Button from '@/ui/button/Button'
+import Field from '@/ui/input/Field'
 
 import { IAuthResponse, IEmailPassword } from '@/store/user/user.interface'
 
 import { useActions } from '@/hooks/useActions'
 import { useAuth } from '@/hooks/useAuth'
 
+import { useAuthRedirect } from './useAuthRedirect'
+import { validEmail } from './valid-email'
+import { validPasword } from './valid-password'
+
 const Auth: FC = () => {
+	useAuthRedirect()
+
 	const { isLoading } = useAuth()
 
 	const { login, register } = useActions()
@@ -20,7 +28,7 @@ const Auth: FC = () => {
 	const {
 		register: formRegister,
 		handleSubmit,
-		formState,
+		formState: { errors },
 		reset
 	} = useForm<IEmailPassword>({
 		mode: 'onChange'
@@ -35,11 +43,55 @@ const Auth: FC = () => {
 
 	return (
 		<Meta title='Auth'>
-			<Heading className='capitalize'>{type}</Heading>
-
-			<form onSubmit={handleSubmit(onSubmit)}>
-				<Button variant='orange'>Let's go!</Button>
-			</form>
+			<section className='flex h-screen'>
+				<form
+					onSubmit={handleSubmit(onSubmit)}
+					className='rounded-lg bg-white shadow-sm p-8 m-auto'
+				>
+					<Heading className='capitalize text-center mb-4'>{type}</Heading>
+					{/*TODO: Сделать ролик загрузки */}
+					{/* {isLoading ? (
+						<Loader />
+					) : (
+						<> */}
+					<Field
+						{...formRegister('email', {
+							required: 'Email is required',
+							pattern: {
+								value: validEmail,
+								message: 'Please enter a valid email address'
+							}
+						})}
+						placeholder='Email'
+						error={errors.email?.message}
+					/>
+					<Field
+						{...formRegister('password', {
+							required: 'Password is required',
+							minLength: {
+								value: 6,
+								message: 'Min length should more 6 symbols'
+							}
+						})}
+						type='password'
+						placeholder='Password'
+						error={errors.password?.message}
+					/>
+					<Button type='submit' variant='orange'>
+						Let's go!
+					</Button>
+					<button
+						type='button'
+						className='inline-block opacity-50 mt-3 ml-3
+                '
+						onClick={() => setType(type === 'login' ? 'register' : 'login')}
+					>
+						{type === 'login' ? 'Register' : 'Login'}
+					</button>
+					{/* </> */}
+					{/* )} */}
+				</form>
+			</section>
 		</Meta>
 	)
 }
---
+++ src/components/ui/input/Field.tsx
@@ -0,0 +1,41 @@
+import cn from 'clsx'
+import { forwardRef } from 'react'
+
+import { IField } from './field.interface'
+
+const Field = forwardRef<HTMLInputElement, IField>(
+	(
+		{ placeholder, error, className, type = 'text', style, Icon, ...rest },
+		ref
+	) => {
+		return (
+			<div className={cn('mb-4', className)} style={style}>
+				<label className='flex flex-col'>
+					<span className='mb-1 block'>
+						{Icon && <Icon className='mr-3' />}
+						{placeholder}
+					</span>
+					<input
+						ref={ref}
+						type={type}
+						{...rest}
+						placeholder={placeholder}
+						className={cn(
+							'px-4 py-2 outline-none border border-gray border-solid focus:border-primary transition-all placeholder:text-gray rounded-lg',
+							{
+								'border-red': !!error
+							}
+						)}
+						w-full
+						outline-none
+					/>
+				</label>
+				{error && <div className='text-red mt-1 text-sm'>{error}</div>}
+			</div>
+		)
+	}
+)
+
+Field.displayName = 'Field' //?
+
+export default Field
